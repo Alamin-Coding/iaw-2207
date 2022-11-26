24cb60167295e39eb7bd3f36180cc2d070c7adc3
Alamin-Coding <alamin.codelover@gmail.com>
2022-11-26 15:52:08 +0600

    all file and folder added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".popular_slider").slick({
+        slidesToShow:3,
+        arrows:false,
+        centerMode:true,
+        centerPadding: "150px",
+        focusOnSelect:true,
+    })
+})
