50932ee2e3349ba794d3604cb8055d07da2dc94e
Alamin-Coding <alamin.codelover@gmail.com>
2022-12-03 15:56:58 +0600

    Conter Up and CountDown add

--- js/script.js
+++ js/script.js
@@ -6,4 +6,39 @@ $(function(){
         centerPadding: "150px",
         focusOnSelect:true,
     })
+    // Counter Up Js
+    $('.counter').counterUp({
+        time: 3000,
+        delay: 10
+    });
+
+
+    // Count Down js
+//     let days = document.querySelector(".days")
+//     let hours = document.querySelector(".hours")
+//     let minutes = document.querySelector(".minutes")
+//     let seconds = document.querySelector(".seconds")
+// let countDownDate = new Date("Dec 3, 2023 15:30:15").getTime();
+
+// // Update the count down every 1 second
+//     let x = setInterval(function() {
+//         let now = new Date().getTime();
+//         let distance = countDownDate - now;
+    
+//         // Time calculations for days, hours, minutes and seconds
+//         let d = Math.floor(distance / (1000 * 60 * 60 * 24));
+//         let h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//         let m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//         let s = Math.floor((distance % (1000 * 60)) / 1000);
+//         days.innerHTML = d;
+//         hours.innerHTML = h;
+//         minutes.innerHTML = m;
+//         seconds.innerHTML = s;
+        
+//         if (distance < 0) {
+//             clearInterval(x);
+//             document.getElementById("demo").innerHTML = "EXPIRED";
+//         }
+// }, 1000);
+
 })
